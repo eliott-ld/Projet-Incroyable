fea4e4cb6ad8ad460e54608764a5ed2b2dc1ae76
eliott-ld <ledauphineliott@gmail.com>
2024-12-13 14:04:34 +0100

    13/12

--- main.cc
+++ main.cc
@@ -3,6 +3,8 @@
 int main(){
     bool start=true;
     int tour = 0;
+    bool choix = 0;
+    int quantity = 0;
     if(tour == 0){
         printf("Bienvenue dans le jeu Fantastic Factory,\n Bonne production !\n");
         printf("Règles du jeu : Vous etes propiétaire d'une usine de production. Vous fournissez des supermarchés ayant une demande infinie !\n");
@@ -11,12 +13,46 @@ int main(){
     while(start){
         printf("Tour %d\n", tour);
         printf("Vous etes actuellement au niveau : %d\n", company.getLevel());
-        printf("Vous avez actuellement %d euros\n", company.getBalance());
-        printf("Vous avez actuellement %d employés\n", company.getNbEmployees());
-        printf("Vous avez actuellement %d matières premières\n", company.getNbRawMaterials());
+        printf("Vous avez actuellement %d pièces magiques\n", company.getBalance());
+        printf("Vous avez actuellement %d elfs employés\n", company.getNbEmployees());
+        printf("Vous avez actuellement %d kg de métal\n", metal.getNbRawMaterials());
+        printf("Vous avez actuellement %d kg de plastique\n", plastic.getNbRawMaterials());
         printf("-------------------------------------------------------------------\n");
-        printf("1. Acheter des matières premières\n");
+
+
+        //MATIERE PREMIERE
+        printf("1. Souhaitez-vous acheter des matières premières ?\n 1(OUI) ou 0(NON) ?\n");
+        scanf("%d", choix);
+        if(choix==1){
+            printf("Quelle de matière première voulez-vous acheter ?\n");
+            printf("0. Metal\n 1. Plastique\n");
+            scanf("%d", choix);
+            if (choix == 0){
+                printf("Combien de kg de métal voulez-vous acheter ?\n");
+                scanf("%d", quantity);
+                metal.buyRawMaterials(quantity);
+            }
+            
+            else{
+                printf("Combien de kg de plastique voulez-vous acheter ?\n");
+                scanf("%d", quantity);
+                metal.buyRawMaterials(quantity);
+            }
+            
+        }
+
+
+
+        //PRODUCTION
         printf("2. Produire des produits\n");
+        printf("Souhaitez-vous changer la production?\n  1(OUI) ou 0(NON) ?\n");
+
+
+
+
+
+
+        //ENGAGER UN EMPLOYE
         printf("3. Engager un employé\n");
 
 
